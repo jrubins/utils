939158e46dabf38431f583fc7bb855f1f3efaba8
Jonathan Rubins <jonrubins@gmail.com>
2018-11-22 08:11:37 -0500

    Added offset DOM util.

--- src/dom/index.js
+++ src/dom/index.js
@@ -10,11 +10,7 @@ import _ from 'lodash'
  * @returns {DOMElement}
  */
 export function generateLink(opts) {
-  const {
-    href,
-    onloadFn = null,
-    rel = 'stylesheet',
-  } = opts
+  const { href, onloadFn = null, rel = 'stylesheet' } = opts
   const link = document.createElement('link')
   link.rel = rel
   link.href = href
@@ -34,9 +30,7 @@ export function generateLink(opts) {
  * @returns {DOMElement}
  */
 export function generateScript(opts) {
-  const {
-    scriptText,
-  } = opts
+  const { scriptText } = opts
   const script = document.createElement('script')
   script.appendChild(document.createTextNode(scriptText))
 
@@ -51,9 +45,7 @@ export function generateScript(opts) {
  * @returns {DOMElement}
  */
 export function generateStyle(opts) {
-  const {
-    css,
-  } = opts
+  const { css } = opts
   const style = document.createElement('style')
   style.type = 'text/css'
   style.appendChild(document.createTextNode(css))
@@ -71,12 +63,7 @@ export function generateStyle(opts) {
  * @param {String} opts.src
  */
 export function insertScript(opts) {
-  const {
-    async = true,
-    id,
-    onloadFn = null,
-    src,
-  } = opts
+  const { async = true, id, onloadFn = null, src } = opts
 
   if (document.getElementById(id)) {
     return
@@ -94,4 +81,36 @@ export function insertScript(opts) {
 
   const firstJsEl = document.getElementsByTagName('script')[0]
   firstJsEl.parentNode.insertBefore(scriptEl, firstJsEl)
+}
+
+/**
+ * Gets the offset (top, left) of the provided DOM element relative to the document.
+ *
+ * @param {Element} el
+ * @returns {{top: Number, left: Number}}
+ */
+export function offset(el) {
+  const box = el.getBoundingClientRect()
+
+  const scrollTop =
+    window.pageYOffset ||
+    document.documentElement.scrollTop ||
+    document.body.scrollTop
+  const scrollLeft =
+    window.pageXOffset ||
+    document.documentElement.scrollLeft ||
+    document.body.scrollLeft
+
+  const clientTop =
+    document.documentElement.clientTop || document.body.clientTop || 0
+  const clientLeft =
+    document.documentElement.clientLeft || document.body.clientLeft || 0
+
+  const top = box.top + scrollTop - clientTop
+  const left = box.left + scrollLeft - clientLeft
+
+  return {
+    top: Math.round(top),
+    left: Math.round(left),
+  }
 }
